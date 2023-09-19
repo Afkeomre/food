f2dc08459972cccfbaee53f4c0d450f7adccd864
Afkeomre <voronk0va.ir@yandex.ru>
2023-09-19 13:37:25 +0300

    add request

--- js/script.js
+++ js/script.js
@@ -113,7 +113,7 @@ window.addEventListener('DOMContentLoaded', () => {
       }
    });
 
-   //const modalTimerId = setTimeout(openModal, 5000);
+   const modalTimerId = setTimeout(openModal, 5000);
 
    window.addEventListener('scroll', showModalByScroll);
 
@@ -209,4 +209,55 @@ window.addEventListener('DOMContentLoaded', () => {
       12,
       '.menu .container'
    );
+
+   //Forms
+   const forms = document.querySelectorAll('form');
+   const message = {
+      loading: 'Загрузка',
+      success: 'Спасибо! Скоро мы с вами свяжемся',
+      failure: 'Что-то пошло не так...',
+   };
+
+   forms.forEach((item) => {
+      postData(item);
+   });
+
+   function postData(form) {
+      form.addEventListener('submit', (e) => {
+         e.preventDefault();
+
+         const statusMessage = document.createElement('div');
+         statusMessage.classList.add('status');
+         statusMessage.textContent = message.loading;
+         form.append(statusMessage);
+
+         const request = new XMLHttpRequest();
+         request.open('POST', 'server.php');
+
+         request.setRequestHeader('Content-type', 'application/json');
+         const formData = new FormData(form);
+
+         const object = {};
+         formData.forEach(function (value, key) {
+            object[key] = value;
+         });
+
+         const json = JSON.stringify(object);
+
+         request.send(json);
+
+         request.addEventListener('load', () => {
+            if (request.status === 200) {
+               console.log(request.response);
+               statusMessage.textContent = message.success;
+               form.reset();
+               setTimeout(() => {
+                  statusMessage.remove();
+               }, 2000);
+            } else {
+               statusMessage.textContent = message.failure;
+            }
+         });
+      });
+   }
 });
